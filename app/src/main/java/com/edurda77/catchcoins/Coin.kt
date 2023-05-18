c55d03eddf9b83361143d5852ebd529bbf588667
HayarovEd <edurd@rambler.ru>
2023-05-18 05:00:59 +0300

    1

--- app/src/main/java/com/edurda77/catchcoins/Coin.kt
+++ app/src/main/java/com/edurda77/catchcoins/Coin.kt
@@ -1,14 +1,39 @@
 package com.edurda77.catchcoins
 
+import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import java.util.Random
 
-data class Coin(
-    val coins: List<Bitmap>,
-    val coinFrame: Int = 0,
-    val coinX: Int,
-    val coinY: Int,
-    val coinVelocity: Int,
-    val random: Random
-)
+class Coin(context: Context) {
+    var coins = arrayOfNulls<Bitmap>(4)
+    var coinFrame = 0
+    var coinX = 0
+    var coinY = 0
+    var coinVelocity = 0
+    var random: Random
 
+    init {
+        coins[1] = BitmapFactory.decodeResource(context.resources, R.drawable.moneta1)
+        coins[2] = BitmapFactory.decodeResource(context.resources, R.drawable.moneta2)
+        coins[3] = BitmapFactory.decodeResource(context.resources, R.drawable.moneta3)
+        coins[4] = BitmapFactory.decodeResource(context.resources, R.drawable.moneta4)
+        random = Random()
+        resetPosition()
+    }
+
+    fun getCoin(coinFrame: Int): Bitmap? {
+        return coins[coinFrame]
+    }
+
+    val coinWidth: Int
+        get() = coins[0]!!.width
+    val coinHeight: Int
+        get() = coins[0]!!.height
+
+    fun resetPosition() {
+        coinX = random.nextInt(GameView.dWidth - coinWidth)
+        coinY = -200 + random.nextInt(600) * -1
+        coinVelocity = 34 + random.nextInt(16)
+    }
+}
